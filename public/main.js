1c71eeeab472189a2a8bf58508feaf35beb7456d
billylin1954 <132108254+billylin1954@users.noreply.github.com>
2024-09-28 19:19:03 -0400

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,12 @@
+var clicks=0;
+var click_string="";
+function clicker(){
+   clicks+=1;
+  console.log("working")
+   document.getElementById("ramen_count").innerHTML=clicks;
+}
+function offclick(){
+      getElementById("ramen").style.height="200px";
+      getElementById("ramen").style.width="200px";
+}
+
